51151ba71b12a7c300588dc9ddc9055862103b8d
Ben Mishali <ben9992@gmail.com>
2017-06-28 11:01:26 +0300

    new one

--- assets/js/contrillers/homepageController.js
+++
@@ -1,4 +0,0 @@
-var myModule = angular.module('app');
-myModule.controller('homepageController', ['$scope', '$location', '$http', function($scope, $location, $http) {
-    
-}]);
--- assets/js/contrillers/signupController.js
+++
@@ -1,6 +0,0 @@
-var myModule = angular.module('app');
-myModule.controller('signupController', ['$scope', '$http', function($scope, $http) {
-    
-  $scope.aaa = 'signup page';
-  
-}]);
